f99c10fd26bd19108021d79d7c343b80cee4ec6b
hexatester <revolusi147id@gmail.com>
2021-03-19 18:43:05 +0700

    Refactor init and auth of Tokopedia to BaseTokopedia

--- tokopedia/__init__.py
+++ tokopedia/__init__.py
@@ -1,7 +1,8 @@
 from .access_token import AccessToken
+from .base import BaseTokopedia
 from .tokopedia import Tokopedia
 from .version import __version__  # NOQA
 
 __author__ = "hexatester <hexatester@protonmail.com>"
 
-__all__ = ["AccessToken", "Tokopedia"]
+__all__ = ["AccessToken", "BaseTokopedia", "Tokopedia"]
---
+++ tokopedia/base.py
@@ -0,0 +1,33 @@
+from typing import Optional
+
+from . import AccessToken
+from .constants import BASE_URL
+
+
+class BaseTokopedia(object):
+    def __init__(
+        self,
+        client_id: str,
+        client_secret: str,
+        access_token: Optional[AccessToken] = None,
+    ):
+        self._client_id = client_id
+        self._client_secret = client_secret
+        self._access_token = access_token or AccessToken.create(
+            client_id=client_id,
+            client_secret=client_secret,
+        )
+
+    @property
+    def client_id(self) -> str:
+        return self._client_id
+
+    @property
+    def access_token(self) -> AccessToken:
+        if not self._access_token:
+            self._access_token.update(
+                client_id=self.client_id,
+                client_secret=self._client_secret,
+            )
+            return self._access_token
+        return self._access_token
