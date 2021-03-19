02506514dddf1189ad3af4828df034e36ccd6358
hexatester <revolusi147id@gmail.com>
2021-03-19 17:48:36 +0700

    Initial Tokopedia

--- tokopedia/__init__.py
+++ tokopedia/__init__.py
@@ -1,6 +1,7 @@
 from .access_token import AccessToken
+from .tokopedia import Tokopedia
 from .version import __version__  # NOQA
 
 __author__ = "hexatester <hexatester@protonmail.com>"
 
-__all__ = ["AccessToken"]
+__all__ = ["AccessToken", "Tokopedia"]
---
+++ tokopedia/tokopedia.py
@@ -0,0 +1,26 @@
+from . import AccessToken
+from .constants import BASE_URL
+
+
+class Tokopedia:
+    def __init__(self, client_id: str, client_secret: str):
+        self._client_id = client_id
+        self._client_secret = client_secret
+        self._access_token = AccessToken.create(
+            client_id=client_id,
+            client_secret=client_secret,
+        )
+
+    @property
+    def client_id(self) -> str:
+        return self._client_id
+
+    @property
+    def access_token(self) -> AccessToken:
+        if not self._access_token:
+            self._access_token = AccessToken.create(
+                client_id=self.client_id,
+                client_secret=self._client_secret,
+            )
+            return
+        return self._access_token
