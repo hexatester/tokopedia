57380452af1f8c42c281fa829d25035ded91411b
hexatester <revolusi147id@gmail.com>
2021-03-21 10:38:36 +0700

    Initial ProductApi.get_product_by_url

--- tokopedia/product/api.py
+++ tokopedia/product/api.py
@@ -13,3 +13,11 @@ class ProductApi(BaseTokopedia):
         )
         data = json.loads(res.text)
         return cattr.structure(data, Product)
+
+    def get_product_by_url(self, fsd_id: int, product_url: int):
+        res = self.session.get(
+            url=self.url(f"/inventory/v1/fs/{fsd_id}/product/info"),
+            query={"product_url": product_url},
+        )
+        data = json.loads(res.text)
+        return cattr.structure(data, Product)
