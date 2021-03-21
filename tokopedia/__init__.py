25fc7b06aeb23cf94c6d58dc8fe259deff42f1ab
hexatester <revolusi147id@gmail.com>
2021-03-21 11:55:41 +0700

    Added ProductApi to Tokopedia.product

--- tokopedia/__init__.py
+++ tokopedia/__init__.py
@@ -1,6 +1,9 @@
 from .access_token import AccessToken
 from .response import TokopediaResponse, TokopediaResponseV2
 from .base import BaseTokopedia
+
+from .product import ProductApi
+
 from .tokopedia import Tokopedia
 from .version import __version__  # NOQA
 
@@ -11,5 +14,6 @@ __all__ = [
     "TokopediaResponse",
     "TokopediaResponseV2",
     "BaseTokopedia",
+    "ProductApi",
     "Tokopedia",
 ]
--- tokopedia/tokopedia.py
+++ tokopedia/tokopedia.py
@@ -1,5 +1,36 @@
+from typing import Optional
+
 from . import BaseTokopedia
+from . import ProductApi
 
 
 class Tokopedia(BaseTokopedia):
-    pass
+    def __init__(
+        self,
+        client_id,
+        client_secret,
+        access_token=None,
+        session=None,
+        base_url=BASE_URL,
+    ):
+        super().__init__(
+            client_id,
+            client_secret,
+            access_token=access_token,
+            session=session,
+            base_url=base_url,
+        )
+        self._product: Optional[ProductApi] = None
+
+    @property
+    def product(self) -> ProductApi:
+        if self._product:
+            return self._product
+        self._product = ProductApi(
+            self.client_id,
+            self._client_secret,
+            self.access_token,
+            self.session,
+            self.base_url,
+        )
+        return self._product
