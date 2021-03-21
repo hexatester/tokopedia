c721c74f97af4caf8925faccd80127418c3c906d
hexatester <revolusi147id@gmail.com>
2021-03-21 10:54:33 +0700

    Initial TokopediaResponse

--- tokopedia/__init__.py
+++ tokopedia/__init__.py
@@ -1,8 +1,14 @@
 from .access_token import AccessToken
+from .response import TokopediaResponse
 from .base import BaseTokopedia
 from .tokopedia import Tokopedia
 from .version import __version__  # NOQA
 
 __author__ = "hexatester <hexatester@protonmail.com>"
 
-__all__ = ["AccessToken", "BaseTokopedia", "Tokopedia"]
+__all__ = [
+    "AccessToken",
+    "TokopediaResponse",
+    "BaseTokopedia",
+    "Tokopedia",
+]
---
+++ tokopedia/response.py
@@ -0,0 +1,27 @@
+import attr
+from typing import Generic, TypeVar, Union
+
+
+@attr.dataclass(slots=True)
+class BaseResponseHeader:
+    process_time: float
+    messages: str
+
+
+class ResponseHeader(BaseResponseHeader):
+    pass
+
+
+@attr.dataclass(slots=True)
+class ErrorResponseHeader(BaseResponseHeader):
+    reason: str
+    error_code: str
+
+
+T = TypeVar("T")
+
+
+@attr.dataclass(slots=True)
+class TokopediaResponse(Generic[T]):
+    header: Union[ResponseHeader, ErrorResponseHeader]
+    data: T
