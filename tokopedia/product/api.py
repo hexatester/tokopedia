69beacd246a3262d17de7ceff53f41bd483e55a8
hexatester <revolusi147id@gmail.com>
2021-03-21 10:37:40 +0700

    Initial ProductApi.get_product_by_id

--- tokopedia/product/api.py
+++ tokopedia/product/api.py
@@ -1,5 +1,15 @@
+import cattr
+import json
 from tokopedia import BaseTokopedia
+
+from . import Product
 
 
 class ProductApi(BaseTokopedia):
-    pass
+    def get_product_by_id(self, fsd_id: int, product_id: int):
+        res = self.session.get(
+            url=self.url(f"/inventory/v1/fs/{fsd_id}/product/info"),
+            query={"product_id": product_id},
+        )
+        data = json.loads(res.text)
+        return cattr.structure(data, Product)
