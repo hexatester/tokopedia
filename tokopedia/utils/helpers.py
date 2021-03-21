0c39d6bc0a157ad895751374cfd4385c0442bfe6
hexatester <revolusi147id@gmail.com>
2021-03-21 12:14:24 +0700

    Delete unused utils

--- tokopedia/utils/helpers.py
+++
@@ -1,8 +0,0 @@
-from datetime import datetime
-from typing import Union
-
-
-def int_to_datetime(value: Union[int, float]) -> datetime:
-    if isinstance(value, float):
-        return datetime.fromtimestamp(value)
-    return datetime.fromtimestamp(float(value))
