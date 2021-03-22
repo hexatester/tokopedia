8a4e2e83956324bf5585305752aa7f598558dc4c
hexatester <revolusi147id@gmail.com>
2021-03-22 23:46:03 +0700

    Initial test_get_variant_by_category_id

---
+++ tests/test_product/test_variant_category.py
@@ -0,0 +1,45 @@
+import cattr
+from tokopedia.response import ResponseHeader
+from tokopedia.product import ResponseVariantCategory, VariantCategory
+from tokopedia.product.variant_category import VariantUnit
+
+
+def test_get_variant_by_category_id():
+    data = {
+        "header": {
+            "process_time": 0.755010807,
+            "messages": "Your request has been processed successfully",
+        },
+        "data": [
+            {
+                "variant_id": 6,
+                "name": "Ukuran Pakaian",
+                "identifier": "size",
+                "status": 1,
+                "has_unit": 1,
+                "units": [
+                    {
+                        "unit_id": 7,
+                        "name": "International",
+                        "short_name": "Intl",
+                        "values": [
+                            {
+                                "value_id": 22,
+                                "value": "XXS",
+                                "hex_code": "",
+                                "icon": "",
+                            },
+                            {"value_id": 23, "value": "XS", "hex_code": "", "icon": ""},
+                        ],
+                    }
+                ],
+            }
+        ],
+    }
+    res: ResponseVariantCategory = cattr.structure(data, ResponseVariantCategory)
+    assert isinstance(res.header, ResponseHeader)
+    assert isinstance(res, ResponseVariantCategory)
+    for variant in res.data:
+        assert isinstance(variant, VariantCategory)
+        for unit in variant.units:
+            assert isinstance(unit, VariantUnit)
