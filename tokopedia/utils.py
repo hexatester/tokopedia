5e20f4e50c77c322a84ed4d2d2bc291b9779e0dc
hexatester <revolusi147id@gmail.com>
2021-03-22 22:57:55 +0700

    Improve register_hooks

--- tokopedia/utils.py
+++ tokopedia/utils.py
@@ -1,7 +1,16 @@
 import cattr
 from datetime import datetime
 from dateutil.parser import parse as parse_datetime
+from typing import Any
+
+
+def make_time(val: None) -> Any:
+    if isinstance(val, (int, float)):
+        return datetime.fromtimestamp(val)
+    if isinstance(val, (str, bytes)):
+        return parse_datetime(val)
+    return val
 
 
 def register_hooks():
-    cattr.register_structure_hook(datetime, lambda x, t: parse_datetime(x))
+    cattr.register_structure_hook(datetime, lambda x, t: make_time(x))
