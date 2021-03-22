88b09b3c669ccf85208f359beb9ccf51e607b252
hexatester <revolusi147id@gmail.com>
2021-03-22 22:24:27 +0700

    Initial GetProductVariantApi.get_variant_by_product_id

--- tokopedia/product/api/get_product_variant.py
+++ tokopedia/product/api/get_product_variant.py
@@ -1,5 +1,5 @@
 from tokopedia import BaseTokopedia
-from tokopedia.product import ResponseVariantCategory
+from tokopedia.product import ResponseVariantCategory, ResponseVariantProduct
 
 
 class GetProductVariantApi(BaseTokopedia):
@@ -19,4 +19,22 @@ class GetProductVariantApi(BaseTokopedia):
             url=self._url(f"/inventory/v1/fs/{fs_id}/category/get_variant"),
             query={"cat_id": cat_id},
             cl=ResponseVariantCategory,
+        )
+
+    def get_variant_by_product_id(
+        self, fs_id: int, product_id: int
+    ) -> ResponseVariantProduct:
+        """get_variant_by_product_id This endpoint retrieves a list of variants related to a product_id.
+
+        Args:
+            fs_id (int): Fulfillment service id.
+            product_id (int): Product id.
+
+        Returns:
+            ResponseVariantProduct: list of variants related to a product_id.
+        """
+        return self._get(
+            url=self._url(f"GET /inventory/v1/fs/{fs_id}/product/variant/{product_id}"),
+            query=None,
+            cl=ResponseVariantProduct,
         )
